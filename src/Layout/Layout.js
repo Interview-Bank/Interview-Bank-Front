ac172af648231cb5ea159872974747c41b52dbcb
Minus960703 <cgzoo9607@gmail.com>
2023-03-27 14:22:53 +0900

    [Refactor] Css update

---
+++ src/Layout/Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderContainer from "./Header/HeaderContainer";
+import Footer from "./Footer/Footer";
+
+const Layout = (props) => {
+  return (
+    <>
+      <HeaderContainer />
+      <MainWrapper>{props.children}</MainWrapper>
+      <Footer />
+    </>
+  );
+};
+
+const MainWrapper = styled.div`
+  position: relative;
+  top: 100px;
+  min-height: calc(100vh - 100px - 60px - 151px);
+`;
+
+export default Layout;
