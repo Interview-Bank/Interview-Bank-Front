02e4309e4ac887808fb45b328cc97e7821ebf59f
Minus960703 <cgzoo9607@gmail.com>
2023-03-29 13:54:46 +0900

    [Feat] Dummy Data And List View Create And Category Stop...
    Data Confirm And Restart Search

--- src/Pages/Search/SearchView.jsx
+++ src/Pages/Search/SearchView.jsx
@@ -243,6 +243,167 @@ const SearchAreaItem = ({ item }) => {
 	);
 };
 
+const categoryData = [
+  {
+    id: 1,
+    category: 'develope',
+    name: '개발'
+  },
+  {
+    id: 2,
+    category: 'develope',
+    name: '기획'
+  },
+  {
+    id: 3,
+    category: 'develope',
+    name: 'R&D'
+  },
+  {
+    id: 4,
+    category: 'develope',
+    name: '디자인'
+  },
+  {
+    id: 5,
+    category: 'develope',
+    name: '마케팅'
+  },
+  {
+    id: 6,
+    category: 'develope',
+    name: '기타'
+  }
+];
+
+const searchArrayList = [
+  {
+    "createdAt": "2023-03-29T04:46:54.021Z",
+    "interviewId": 0,
+    "nickname": "aaaa",
+    "title": "네이버 면접 2022"
+  },
+  {
+    "createdAt": "2023-03-29T04:46:54.021Z",
+    "interviewId": 1,
+    "nickname": "aaaa",
+    "title": "쿠팡 면접 2022"
+  },
+  {
+    "createdAt": "2023-03-29T04:46:54.021Z",
+    "interviewId": 2,
+    "nickname": "aaaa",
+    "title": "야놀자 면접 2022"
+  },
+  {
+    "createdAt": "2023-03-29T04:46:54.021Z",
+    "interviewId": 3,
+    "nickname": "aaaa",
+    "title": "카카오 면접 2022"
+  },
+  {
+    "createdAt": "2023-03-29T04:46:54.021Z",
+    "interviewId": 4,
+    "nickname": "aaaa",
+    "title": "다음 면접 2022"
+  },
+];
+
+const SearchCategory = () => {
+  return (
+    <>
+      <h2>직무 구분</h2>
+      {categoryData &&
+        categoryData.map((current) =>
+          <SearchCategoryCheckBox data={current} key={current.id} />
+        )
+      }
+    </>
+  )
+}
+
+const SearchCategoryCheckBox = ({ data }) => {
+  const { name, category } = data;
+  return (
+    <div className='check__area'>
+      <input type="checkbox" name={category} id={category} />
+      <label for={category}>{name}</label>
+      <style jsx>{`
+        .check__area {
+          display: flex;
+          width: 100%;
+          margin-bottom: 10px;
+          align-items: center;
+        }
+        .check__area > label {
+          padding-left: 13px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+const SearchArea = () => {
+  return (
+    <div className='search__area'>
+      {searchArrayList.length
+        ? <div className="search-list">
+            {searchArrayList.length && searchArrayList.map((current) =>
+              <div className="search__item" key={current.interviewId}>
+                <h4>{current.title}</h4>
+              </div>
+            )}
+          </div>
+        : <div className="search-empty">
+            <h4>
+              요청하신 조건에 맞는 검색 결과가 없습니다.<br />
+              다른 키워드로 검색하시거나, 필터 옵션을 변경해 주세요.
+            </h4>
+          </div>
+      }
+      <style jsx>{`
+        .search__area {
+          width: 68%;
+          min-height: calc(100vh - 60px - 151px);
+        }
+        .search-empty {
+          text-align: center;
+          width: 100%;
+          height: 100%;
+          border-radius: 16px;
+          border: 1px solid #D9D9D9;
+          background-color: white;
+          color: #ABABAB;
+          font-size: 24px;
+          padding-top: 12rem;
+        }
+        .search-empty > span {
+          width: 100%;
+        }
+        .search-list {
+          display: grid;
+          width: 100%;
+          grid-template-columns: repeat(3, 1fr);
+          gap: 30px;
+        }
+        .search__item {
+          width: calc(100% - (29px * 2));
+          aspect-ratio: 1/1.2D;
+          background-color: white;
+          border-radius: 20px;
+          border: 1px solid #d9d9d9;
+          padding: 29px;
+          font-size: 24px;
+          font-weight: 600;
+        }
+        .search__item > h4{
+          margin-top: 200px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
 const SearchInterviewView = () => {
 	const [searchParam, setSearchParam] = useState({
 		title: "",
