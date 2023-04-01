3f94cd27284fa4769725900f9e5135bb332edb64
LEEILLJIN <qjsro368@naver.com>
2023-04-01 17:19:41 +0900

    fix/Remove unused imports and variables

--- src/Pages/Login/LoginContainer.js
+++ src/Pages/Login/LoginContainer.js
@@ -2,15 +2,12 @@ import axios from "axios";
 import LoginView from "./LoginView";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
-import { setToken, setUserId } from "../../Redux/Reducers/AuthReducer.js";
-import { useDispatch } from "react-redux";
 import React, { useState } from "react";
 import { setCookie, setCookieExpires } from '../api/loginApi';
 
 const LoginContainer = () => {
   const API_URL = "https://bstaging.interviewbank.net/";
   const navigate = useNavigate();
-  const dispatch = useDispatch();  
 
   const [loginError, setLoginError] = useState({})
   const loginSubmit = async (values) => {
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import RegisterSelect from "../Pages/Register/RegisterSelect";
 import RegisterEmailContainer from "../Pages/Register/RegisterEmail/RegisterEmailContianer";
