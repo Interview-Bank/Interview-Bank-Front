e41d010f301261a8b407642a8dbb9e56a3e4b9d7
LEEILLJIN <qjsro368@naver.com>
2023-04-30 00:33:54 +0900

    feature/modify find password page, but EmailTransmit function has not been complited

--- src/Pages/FindPassword/FindPasswordContainer.js
+++ src/Pages/FindPassword/FindPasswordContainer.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, {useState, useCallback, useEffect} from "react";
 import FindPasswordView from "./FindPasswordView";
+import axios from "axios";
+
 
 const FindPasswordContainer = () => {
-  return <FindPasswordView />;
+  const API_URL = "https://bstaging.interviewbank.net/account/";
+
+  const [email, setEmail] = useState("");
+  const [emailMsg, setEmailMsg] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+
+
+  const validateEmail = (email) => {
+    return email
+      .toLowerCase()
+      .match(
+        /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+      );
+  };
+
+  const onChangeEmail = useCallback(async (e) => {
+    const currEmail = e.target.value;
+
+    if (!validateEmail(currEmail)) {
+      setEmailMsg("이메일 형식이 올바르지 않습니다.");
+    } else {
+      setEmailMsg("올바른 이메일 형삭");
+      setIsEmailValid(true)
+      setEmail(currEmail)
+    }
+  }, []);
+  const handleEmailTransmit = async () => {
+    let body = {
+      email: email
+    };
+    try {
+      const response = await axios.post(API_URL + "/account/reset-password/send-email", body);
+      console.log(response)
+    } catch (e) {
+      alert(e.response.data.message + "😭");
+    }
+  }
+  return (
+    <FindPasswordView 
+    onChangeEmail={onChangeEmail}
+    isEmailValid = {isEmailValid}
+    emailMsg = {emailMsg}
+    />
+    
+    );
 };
 
 export default FindPasswordContainer;
