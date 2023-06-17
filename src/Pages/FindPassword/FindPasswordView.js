8aead34381efb475c5398b4a91d7ecf579e714a8
LEEILLJIN <qjsro368@naver.com>
2023-06-18 03:02:54 +0900

    feat/developing find password, now send email api complete

--- src/Pages/FindPassword/FindPasswordView.js
+++ src/Pages/FindPassword/FindPasswordView.js
@@ -2,18 +2,27 @@ import React from "react";
 import styled from "styled-components";
 import Layout from "../../Layout/Layout";
 
-const FindPasswordView = () => {
+const FindPasswordView = ({onChangeEmail, isEmailValid, emailMsg, handleEmailTransmit}) => {
   return (
     <>
       <Layout>
         <FindPasswordContainer>
-          <h1>비밀번호 찾기</h1>
-          <p>
+          <FindPasswordTitle>비밀번호 찾기</FindPasswordTitle>
+          <FindPasswordDesc>
             비밀번호를 잃어버리셨나요? 인터뷰 뱅크에 가입한 이메일을 입력해
             주세요. 이메일을 통해 비밀번호 변경 링크가 전송됩니다.
-          </p>
-          <Input placeholder="이메일을 입력해주세요." />
-          <Button>이메일 전송하기</Button>
+          </FindPasswordDesc>
+          <Input 
+              id="email"
+              type="text"
+              placeholder="이메일을 입력해주세요."
+              onChange={onChangeEmail}
+              autoComplete="off"/>
+          <OutputText className={isEmailValid ? "success" : "error"}>
+              {emailMsg}
+          </OutputText>
+          <Button
+            onClick={handleEmailTransmit}>이메일 전송하기</Button>
         </FindPasswordContainer>
       </Layout>
     </>
@@ -21,46 +30,109 @@ const FindPasswordView = () => {
 };
 
 const FindPasswordContainer = styled.div`
-  margin: 64px auto auto;
-  width: 346px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: auto;
   height: 676px;
   min-height: 100vh;
-  > h1 {
-    font-weight: 700;
-    text-align: center;
-    color: #2e55e7;
-    margin-top: 100px;
-  }
-  > p {
-    font-weight: 600;
-    text-align: center;
-    color: #737373;
-    font-size: 14px;
-  }
+
+
+  margin: 0 auto;
+  margin-top: 80px;
+`;
+const FindPasswordTitle = styled.div`
+  position: relative;
+  width: 160px;
+  height: 35px;
+
+  font-family: 'Noto Sans KR';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 35px;
+
+  text-align: center;
+
+  color: #2E55E7;
+
+  margin-bottom: 18px;
+`;
+
+const FindPasswordDesc = styled.div`
+  width: 396px;
+  height: 116px;
+
+  font-family: 'Noto Sans KR';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 29px;
+  text-align: center;
+
+  color: #737373;
+
+  margin-bottom: 60px;
+
 `;
 const Input = styled.input`
-  width: 319px;
-  height: 50px;
-  border-radius: 5px;
-  border: 1px solid #dee2e6;
-  margin-top: 30px;
-  padding-left: 15px;
+  box-sizing: border-box;
+
+  position: relative;
+  width: 350px;
+  height: 55px;
+
+  background: #FFFFFF;
+  border: 1px solid #2E55E7;
+  border-radius: 8px;
+  padding-left: 16px;
+
+  margin-bottom: 12px;
   ::placeholder {
-    color: #b5b5b5;
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 23px;
+
+    color: #AAAAAA;
   }
 `;
 const Button = styled.button`
-  font-size: 16px;
-  font-weight: 700;
-  width: 338px;
-  height: 52px;
-  border-radius: 5px;
-  padding: 15px 8px;
+  width: 350px;
+  height: 55px;
+  border-radius: 8px;
+  padding: 16px 8px;
   background-color: #2e55e7;
+
+  font-family: 'Noto Sans KR';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 23px;
+  text-align: center;
   color: #fff;
-  margin-top: 20px;
   border: none;
   cursor: pointer;
+
+`;
+
+const OutputText = styled.div`
+  border: none;
+  margin-top: 5px;
+  margin-left: 5px;
+  margin-bottom: 5px;
+  font-size: 12px;
+  text-align: start;
+  &.success {
+    color: #2e55e7;
+    display: none;
+  }
+  &.error {
+    color: red;
+
+    display: block;
+  }
 `;
 
 export default FindPasswordView;
