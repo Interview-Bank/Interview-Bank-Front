698068781be1b8054059daadb324205fc3b578f3
JINU <cgzoo9607@gmail.com>
2023-07-29 09:55:54 +0900

    [Refactor] Refactor HomePage Structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,30 +27,28 @@ const HomePage = ({ interviewList }: InterviewListProps) => {
   const { data, isError, isLoading } = useQuery("interview", () => bringHomeInterviewListData(), { staleTime: 2000 })
 
   return (
-    <>
+    <section className='home'>
       <SeoHead title='인터뷰 뱅크' />
-      <section className='home'>
-        <Banner />
-        <HomeSearch />
-        <div className="home__title">
-				  <h2>최신 인터뷰 글 보기</h2>
-        </div>
-        <div className="home__list">
-          {interviewList?.map((interview) => (
-            <TextComponent
-              key                 = {interview.interviewId}
-              id                  = {interview.interviewId}
-              nickname            = {interview.nickname}
-              title               = {interview.title}
-              firstCategoryName   = {interview.jobCategory.firstLevelName}
-              secondCategoryName  = {interview.jobCategory.secondLevelName}
-              createdAt           = {interview.createdAt.slice(0, 10).replaceAll("-", ".")}
-              type                = "interview"
-            />))
-          }
-        </div>
-      </section>
-    </>
+      <Banner />
+      <HomeSearch />
+      <div className="home__title">
+        <h2>최신 인터뷰 글 보기</h2>
+      </div>
+      <div className="home__list">
+        {interviewList?.map((interview) => (
+          <TextComponent
+            key                 = {interview.interviewId}
+            id                  = {interview.interviewId}
+            nickname            = {interview.nickname}
+            title               = {interview.title}
+            firstCategoryName   = {interview.jobCategory.firstLevelName}
+            secondCategoryName  = {interview.jobCategory.secondLevelName}
+            createdAt           = {interview.createdAt.slice(0, 10).replaceAll("-", ".")}
+            type                = "interview"
+          />))
+        }
+      </div>
+    </section>
   )
 }
 
