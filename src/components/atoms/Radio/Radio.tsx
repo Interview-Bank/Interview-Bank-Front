887ce79af84ab0651e5b46a0127c62bac1851f71
최진우 <cgzoo9607@gmail.com>
2023-08-22 18:59:54 +0900

    [Refactor] Refactoring Radio Props Types

--- src/components/atoms/Radio/Radio.tsx
+++ src/components/atoms/Radio/Radio.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-type Props = {}
+type RadioProps = {
+  id: number;
+}
 
-const Radio = ({id, name, type, searchRadio, isChangeCreatedDateRadio}) => {
+const Radio = ({id, name, type, searchRadio, isChangeCreatedDateRadio}: RadioProps) => {
   return (
     <label htmlFor={id} onClick={()=> isChangeCreatedDateRadio(id)}>
       <input type="radio" name={type} id={id} checked={searchRadio===id ? true : false } />
